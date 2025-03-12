e64f88762cbc43ba16023a1caf56e413d0e4d7c2
Keevin Aroco <gavikde2022@gmail.com>
2025-03-12 17:26:46 +0100

    Add Trip model

--- bkoda/settings.py
+++ bkoda/settings.py
@@ -24,13 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h!ml^+!w-*m610-$iju8fjaa3ra%rm3_gbl8o$3+5vx=f1$%k@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'herokuapp.com',
+    '127.0.0.1',
 ]
 
 
@@ -90,6 +91,11 @@ WSGI_APPLICATION = 'bkoda.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
 # Create your models here.
+class Trip(models.Model):
+    trip_number = models.IntegerField()
+    origin = models.CharField(max_length=30, default='Kabayan')
+    destination = models.CharField(max_length=30, default='Baguio')
+    date = models.DateField()
+    time = models.TimeField()
+    total_seats = models.IntegerField()
+    available_seats = models.IntegerField()
+
+
+ 
