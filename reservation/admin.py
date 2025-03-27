7870a2dbc93da4e57a34471b4f2496ffedc05efa
Keevin Aroco <gavikde2022@gmail.com>
2025-03-27 09:53:17 +0100

    Change reservation count in trip list to seats reserved

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Trip, Reservation
+from django.db.models import Sum
 
 # Register your models here.
 class ReservationInline(admin.TabularInline):
@@ -8,15 +9,18 @@ class ReservationInline(admin.TabularInline):
 
 @admin.register(Trip)
 class TripAdmin(admin.ModelAdmin):
-    list_display = ('trip_number', 'origin', 'destination', 'date', 'time', 'total_seats', 'available_seats', 'reservation_count')
-    search_fields = ('trip_number', 'origin', 'date')
+    list_display = ('trip_number', 'origin', 'destination', 'date', 'time', 'total_seats', 'available_seats', 'total_seats_reserved')
     list_filter = ('origin', 'destination', 'date')
+    search_fields = ('trip_number', 'origin', 'destination')
     inlines = [ReservationInline]
-    ordering = ('date', 'time')
+    ordering = ('-date', '-time')
 
-    def reservation_count(self, obj):
-        return obj.reservation_set.count()
-    reservation_count.short_description = 'Reservations'
+    def total_seats_reserved(self, obj):
+        """Calculates the total seats reserved for a trip."""
+        total_reserved = Reservation.objects.filter(trip=obj).aggregate(total_reserved=Sum('number_of_seats'))['total_reserved'] or 0
+        return total_reserved
+
+    total_seats_reserved.short_description = 'Total Seats Reserved'
 
 
 @admin.register(Reservation)
