970f06f01568a8a3bdf5bdb0f75f8470171883de
Keevin Aroco <gavikde2022@gmail.com>
2025-03-28 17:49:42 +0100

    Add back button on reservation form

--- reservation/static/js/script.js
+++ reservation/static/js/script.js
@@ -1,5 +1,17 @@
+/**
+ * Hides all alert messages on the page after a 5 second delay.
+ */
 setTimeout(function () {
     document.querySelectorAll('.alert').forEach(function (alert) {
         alert.remove();
     });
 }, 5000);
+
+/**
+ * Navigates the browser back to the previous page in history after confirmation.
+ */
+function goBack() {
+    if (confirm("Are you sure you want to go back? Any unsaved changes may be lost.")) {
+        window.history.back();
+    }
+}
